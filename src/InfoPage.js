0608a8c1d15d4716d548ecd47f3914b5a4cbc8a0
Kristy Nguyen <knyen008@gmail.com>
2022-07-15 22:38:01 -0600

    Reformat details on back of movie card

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -33,22 +33,24 @@ class InfoPage extends Component {
         {this.state.isError ? <Error errorMessage={this.state.errorMessage} /> :
           <article className='movie-info-container' style={{backgroundImage: `url(${this.state.selectedMovie.backdrop_path})`}}>
             <div className='info-mask'>
-            <div className="rotating-box">
-              <div className="rotating-box-inner">
-                <div className="rotating-box-front">
-                  <img className='rotating-image'  src={this.state.selectedMovie.poster_path} alt='movie backdrop'></img> 
-                </div>
-                <div className="rotating-box-back">
-                  <h2 className='selected-movie-title'>{this.state.selectedMovie.title}</h2>
-                  <h2 className='selected-movie-tagline'>{this.state.selectedMovie.tagline}</h2>
-                  <h2 className='selected-movie-overview'>{this.state.selectedMovie.overview}</h2>
-                  <h2 className='selected-movie-release-date'>RELEASE DATE: {this.state.selectedMovie.release_date}</h2>
-                  <h2 className='selected-movie-genres'>{this.state.selectedMovie.genres}</h2>
-                  <h2 className='selected-movie-runtime'>RUNTIME: {this.state.selectedMovie.runtime} MINUTES</h2>
-                  <h2 className='selected-movie-average-rating'>{Math.round(this.state.selectedMovie.average_rating * 10) / 10}/10</h2>
+              <div className="rotating-box">
+                <div className="rotating-box-inner">
+                  <div className="rotating-box-front">
+                    <img className='rotating-image'  src={this.state.selectedMovie.poster_path} alt='movie backdrop'></img> 
+                  </div>
+                  <div className="rotating-box-back">
+                    <h2 className='title'>{this.state.selectedMovie.title}</h2>
+                    <h3 className='tagline'>{this.state.selectedMovie.tagline}</h3>
+                    <p className="information">
+                      <b>RELEASE DATE</b>: {this.state.selectedMovie.release_date} <br></br><br></br>
+                      {this.state.selectedMovie.genres} <br></br><br></br>
+                      {Math.round(this.state.selectedMovie.average_rating * 10) / 10}/10 ⭐️ <br></br><br></br>
+                      <b>RUNTIME</b>: {this.state.selectedMovie.runtime} MINUTES
+                    </p>
+                    <h3 className='overview'>{this.state.selectedMovie.overview}</h3>
+                  </div>
                 </div>
               </div>
-            </div>
             </div>
           </article>
         }

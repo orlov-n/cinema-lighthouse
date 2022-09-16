3beaa48be8ec84072249e2efbf13e7da1194d146
Nick Orlov <ialwaysreply@gmail.com>
2022-09-16 13:28:29 -0700

    Update routing

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-//   <BrowserRouter basename={process.env.PUBLIC_URL}>
+//   <BrowserRouter>
 //     <App />
 //   </BrowserRouter>
 // );
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
